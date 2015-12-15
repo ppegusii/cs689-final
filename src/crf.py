7965ec17d3588632262dca5a111f8e21e7e52b72
stephenlee86 <stephenlee86@gmail.com>
2015-12-14 21:34:25 -0500

    crf works!

--- src/crf.py
+++ src/crf.py
@@ -9,71 +9,127 @@ import sys
 import pycrfsuite
 import numpy as np
 
+from sklearn.cross_validation import KFold
+
+data_loc_str = '../data/kasteren/2010/datasets/house{house}/{feature}.csv.gz'
+from collections import Counter
+
+def print_transitions(trans_features):
+    for (label_from, label_to), weight in trans_features:
+        print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
+
+def sensor2features(s,i):
+    sensor = s[i]
+    #features = ['s_'+str(i)+'=' + str(bool(int(sensor[i]))) for i in range(len(sensor))]
+    features = ['i_'+str(i)+'=' + str(sensor[i]) for i in range(len(sensor))]
+    #features = np.append(features, features_neg)
+    return features
+
+def s2features(s):
+    return [sensor2features(s, i) for i in range(len(s))]
+
 def main():
-    args = parseArgs(sys.argv)
-    data = load.data(args.data, dtype_str=True)
-    classify(data)
-    test_model(data)
+    # args = parseArgs(sys.argv)
 
-def classify(data):
+    for house in [ 'B' , 'C']:
+        for f in ['last', 'change', 'data']:
+            loc = data_loc_str.format(house=house,feature=f)
+            #load the data
+            data = load.data(loc, dtype_str=True)
 
-    trainDf, testDf, trainLens, testLens, testFrac = split.trainTest(
-        data, 5400, 5400*2, testSize=0.3)
+            #split data into training and testing
+            #trainDf, testDf, trainLens, testLens, testFrac = split.trainTest(
+            #    data, 5400, 5400*2, testSize=0.3)
 
-    #X_train = trainDf.values[:, :trainDf.shape[1] - 2]
-    #y_train = trainDf.values[:, trainDf.shape[1] - 1]
 
-    X_train = np.array_split(trainDf.values[:, :trainDf.shape[1] - 2], 66290)
-    y_train = np.array_split(trainDf.values[:, trainDf.shape[1] - 1], 66290)
+            X_train = np.array(np.array_split(data.values[:, :data.shape[1] - 2], 10))
+            y_train = np.array(np.array_split(data.values[:, data.shape[1] - 1], 10))
 
-    #X_test = np.array_split(np.array(testDf.values[:, :testDf.shape[1] - 2], dtype=np.uint8), 30)
-    #y_test = np.array_split(np.array(testDf.values[:, testDf.shape[1] - 1], dtype=np.uint8), 30)
+            #### WITHOUT CROSSVALID
+            #X_test = np.array_split(np.array(testDf.values[:, :testDf.shape[1] - 2], dtype=np.uint8),2)
+            #y_test = np.array_split(np.array(testDf.values[:, testDf.shape[1] - 1], dtype=np.uint8),2)
 
-    trainer = pycrfsuite.Trainer(verbose=False)
-    #trainer.append(X_train, y_train)
-    # X_train.shape = [  [sentence feature [ word feature] ] ]
-    # y_train.shape = [   [sentence [ word label] ]   ]
+            #test_CRF(X_train, X_test, y_train, y_test, house, f,999)
+            #exit()
+            #### WITHOUT CROSSVALID
+            kf = KFold(len(X_train), n_folds=5)
+
+            clfs = []
+            accuracies = []
+            # cross validation
+            for i, (train_index, test_index) in enumerate(kf):
+                print("TRAIN:", train_index, "TEST:", test_index)
+                X_train1, X_test1 = X_train[train_index], X_train[test_index]
+                y_train1, y_test1 = y_train[train_index], y_train[test_index]
+
+                train_CRF(X_train1, y_train1, house, f, i)
+
+                clf, accuracy = test_CRF(X_test1, y_test1, house, f, i)
+                clfs.append(clf)
+                accuracies.append(accuracy)
+
+            print accuracies
+
+            #train_model(trainDf)
+            #test_model(testDf)
+
+def train_CRF(X_train, y_train, house, f,i ):
+    X_train = [s2features(s) for s in X_train]
+    trainer = pycrfsuite.Trainer( verbose=False)
+
+    X_train = np.concatenate([np.array_split(x, 25) for x in X_train])
+    y_train = np.concatenate([np.array_split(y, 25) for y in y_train])
+    print len(X_train)
+
+    print X_train[0].shape, y_train[0].shape
+
     for xseq, yseq in zip(X_train, y_train):
-        #print xseq.shape, yseq.shape
-        #print xseq , yseq
         trainer.append(xseq, yseq)
 
-
     trainer.set_params({
-    'c1': 1.0,   # coefficient for L1 penalty
-    'c2': 1e-3,  # coefficient for L2 penalty
-    'max_iterations': 11,  # stop earlier
-
-    # include transitions that are possible, but not observed
-    'feature.possible_transitions': True
+        'max_iterations': 11,  # stop earlier
+        'c1': 1.0,   # coefficient for L1 penalty
+        'c2': 1e-1,  # coefficient for L2 penalty
+        # include transitions that are possible, but not observed
+        'feature.possible_transitions': False
     })
 
-    trainer.train('house_a.crfsuite')
-    print "Model created"
+    model_name = 'crf_models/house_' + house + '_'+ f + str(i) + '.crfsuite'
+    trainer.train(model_name)
+    print str(i),'. House:', house, '. Feature: ', f, ' training complete.'
 
-def test_model(data):
 
-    trainDf, testDf, trainLens, testLens, testFrac = split.trainTest(
-        data, 5400, 5400*2, testSize=0.3)
+def test_CRF( X_test, y_test, house, f, i ):
+    model_name = 'crf_models/house_' + house + '_'+ f + str(i) + '.crfsuite'
 
-    #X_test = testDf.values[:, :testDf.shape[1] - 2]
-    #y_test = testDf.values[:, testDf.shape[1] - 1]
-
-    # using subsequences, splitting the entire month data into days
-    X_test = np.array_split(np.array(testDf.values[:, :testDf.shape[1] - 2], dtype=np.uint8), 30)
-    y_test = np.array_split(np.array(testDf.values[:, testDf.shape[1] - 1], dtype=np.uint8), 30)
-
+    X_test = [s2features(s) for s in X_test]
 
     tagger = pycrfsuite.Tagger()
-    tagger.open('house_a.crfsuite')
-    #print "Predict" , tagger.tag([X_test[5]])
-    #print "Correct", y_test[5]
-    y_pred = [tagger.tag(xseq) for xseq in X_test]
-    compare = zip(y_test, y_pred)
-    correct = [x for x in compare if x[0] == x[1]]
-    print('Accuracy: {}'.format(float(len(correct))/len(compare)))
-    # TODO confusion matrix ;
-    # classificaiton report:
+    tagger.open(model_name)
+
+    y_pred = np.array([])
+    for xseq in X_test:
+        y_pred = np.append(y_pred, np.array(tagger.tag(xseq), dtype=np.uint8))
+
+    y_test = np.array(np.concatenate(y_test), dtype=np.uint8)
+
+
+    accuracy = sum(y_pred == y_test)*1./len(y_pred)
+
+    print('Accuracy: ', accuracy)
+    #print y_pred
+    return house+f+str(i), accuracy
+
+def print_info(tagger):
+    info = tagger.info()
+
+    print("Top likely transitions:")
+    print_transitions(Counter(info.transitions).most_common(15))
+
+    print("\nTop unlikely transitions:")
+    print_transitions(Counter(info.transitions).most_common()[-15:])
+
+    return house+f, accuracy
 
 
 def parseArgs(args):
--- src/ssvm.py
+++ src/ssvm.py
@@ -5,29 +5,55 @@ import argparse
 import load
 import split
 import sys
+from sklearn.cross_validation import KFold
+import pickle
 
-#Using pystruct
+def main():
+    args = parseArgs(sys.argv)
+    data = load.data(args.data)
+
+     #split data into training and testing
+    trainDf, testDf, trainLens, testLens, testFrac = split.trainTest(
+        data, 5400, 5400*2, testSize=0.3)
+
+    classify(data)
+
+
+# Using pystruct
 def test_SSVM(X_train, X_test, y_train, y_test):
+
+    #print X_train.shape, X_train[0].shape
+
+    # splitting the 8 sub-arrays into further:
+    #X_train = np.concatenate([np.array_split(x, 100) for x in X_train])
+    #y_train = np.concatenate([np.array_split(y, 100) for y in y_train])
+
+    #X_test = np.concatenate([np.array_split(x, 30) for x in X_test])
+    #y_test = np.concatenate([np.array_split(y, 30) for y in y_test])
+
+    #print X_train.shape
+    #print X_train[0].shape
+    #print y_train[0].shape
+    #exit()
     #Train using linear chain CRF
     #https://groups.google.com/forum/#!topic/pystruct/KIkF7fzCyDI
 
     model = ChainCRF()
     #ssvm = NSlackSSVM(model=model, C=.1, max_iter=11) # almost similar to FrankWolfeSSVM
-    ssvm = FrankWolfeSSVM(model=model, C=0.01, max_iter=11)
+    ssvm = FrankWolfeSSVM(model=model, C=0.1, max_iter=11)
     # c=0.2 -> 62.86 % accuracy <==> c=0.1
 
     #ssvm = OneSlackSSVM(model=model) #doesn't work as well
     ssvm.fit(X_train, y_train)
     print "Learning complete..."
-    print("Test score with chain CRF: %f" % ssvm.score(X_test, y_test))
+    accuracy = ssvm.score(X_test, y_test)
+    print("Test score with chain CRF: %f" % accuracy )
 
+    return ssvm, accuracy
+
+# used to replace the labels with the new labels that starts with 0;
 def replace(l, label_map):
     return map(lambda x: label_map[int(x)], l)
-
-def main():
-    args = parseArgs(sys.argv)
-    data = load.data(args.data)
-    classify(data)
 
 def relabel(data):
     y = data['activity']
@@ -36,12 +62,14 @@ def relabel(data):
     labels_map = dict( zip(labels, range(len(labels))))
     y = replace(y, labels_map)
     data['activity'] = y
+    print np.unique(y)
     return data
 
 
 def classify(data):
     data = relabel(data)
 
+    # dividing the data into training and testing
     trainDf, testDf, trainLens, testLens, testFrac = split.trainTest(
         data, 5400, 5400*2, testSize=0.3)
 
@@ -49,14 +77,49 @@ def classify(data):
     # X = [np.array([  [f1],[f2],[f3 ] ... [ N days], dtype=uint8 )]
     # Y = [np.array([   a, b , c])]
     # splitting so that we get a fraction of the day for training the labels
-    X_train = np.array_split(trainDf.values[:, :trainDf.shape[1] - 2], 100)
-    y_train = np.array_split(trainDf.values[:, trainDf.shape[1] - 1], 100)
+    X_train = np.array(np.array_split(trainDf.values[:, :trainDf.shape[1] - 2], 10))
+    y_train = np.array(np.array_split(trainDf.values[:, trainDf.shape[1] - 1], 10))
 
-    X_test = np.array_split(np.array(testDf.values[:, :testDf.shape[1] - 2], dtype=np.uint8), 30)
-    y_test = np.array_split(np.array(testDf.values[:, testDf.shape[1] - 1], dtype=np.uint8), 30)
+    # test dataset - dividing into subsequences
+    X_test = np.array(np.array_split(testDf.values[:, :testDf.shape[1] - 2], 30))
+    y_test = np.array(np.array_split(testDf.values[:, testDf.shape[1] - 1], 30))
+    print np.unique(np.concatenate(y_train).ravel())
+    print np.unique(np.concatenate(y_test).ravel())
+
+    #test_SSVM(X_train, X_test, y_train, y_test)
+    #exit()
+    # 5 fold validation;
+    kf = KFold(len(X_train), n_folds=5)
+
+    clfs = []
+    accuracies = []
+    # cross validation
+    for train_index, test_index in kf:
+        print("TRAIN:", train_index, "TEST:", test_index)
+        X_train1, X_test1 = X_train[train_index], X_train[test_index]
+        y_train1, y_test1 = y_train[train_index], y_train[test_index]
+        print X_train1.shape,  y_train1.shape
+        print X_train1[0].shape,  y_train1[0].shape
+
+        clf, accuracy = test_SSVM(X_train1, X_test1, y_train1, y_test1)
+        clfs.append(clf)
+        accuracies.append(accuracy)
+
+    print accuracies
+
+    ssvm = clfs[np.argmax(accuracies)]
+    print "Learning complete..."
+    accuracy = ssvm.score(X_test, y_test)
+    print("Test score with chain CRF: %f" % accuracy )
 
 
-    test_SSVM(X_train, X_test, y_train, y_test)
+    from sklearn.metrics import confusion_matrix
+    confusion_matrix(y_true, y_pred)
+
+    output = open('ssvm_last.pkl', 'wb')
+    pickle.dump(clfs, output)
+
+
     print "Learning SVM complete."
 
 def parseArgs(args):
